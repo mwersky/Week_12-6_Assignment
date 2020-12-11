57747a27ec00664c92516503985cad0a3aa8e7b2
Caleb <calebdgendron@gmail.com>
2020-12-10 20:11:53 -0800

    updating cars entity and carsDao

--- src/entities/Cars.java
+++ src/entities/Cars.java
@@ -8,9 +8,52 @@ public class Cars {
 	private int modelYear;
 	private double price; 
 	
-	public Car(int carId, String model, String make, int modelYear, double price) {
+	public Cars(int carId, String model, String make, int modelYear, double price) {
 		this.setCarId(carId); 
 		this.setModel(model); 
+		this.setMake(make); 
+		this.setModelYear(modelYear); 
+		this.setPrice(price); 
+	}
+
+	public int getCarId() {
+		return carId;
+	}
+
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public int getModelYear() {
+		return modelYear;
+	}
+
+	public void setModelYear(int modelYear) {
+		this.modelYear = modelYear;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
 	}
 	
 }
