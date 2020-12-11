4729b1cb5cf9c31fc4c074f4e7cb41acf03ec97c
Caleb <calebdgendron@gmail.com>
2020-12-10 19:19:33 -0800

    adding cars entity

--- src/entities/Cars.java
+++ src/entities/Cars.java
@@ -2,4 +2,15 @@ package entities;
 
 public class Cars {
 
+	private int carId;
+	private String model; 
+	private String make; 
+	private int modelYear;
+	private double price; 
+	
+	public Car(int carId, String model, String make, int modelYear, double price) {
+		this.setCarId(carId); 
+		this.setModel(model); 
+	}
+	
 }
