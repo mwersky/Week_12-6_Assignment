7b40fe2132d525f6abfebeb74716e6a780ae7e0a
Mitchell Werksy <dungeonmasterwersky@gmail.com>
2020-12-11 17:10:51 -0800

    added up to date customers class.

--- src/entities/Customers.java
+++ src/entities/Customers.java
@@ -1,5 +1,43 @@
 package entities;
 
+
 public class Customers {
 
+	
+	private int customerId;
+	private String firstName;
+	private String lastName;
+
+	
+	public Customers(int customerId, String firstName, String lastName) {
+		this.setCustomerId(customerId);
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+	}
+
+	
+	//Getters and setters.
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
