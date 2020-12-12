d7a61f3b88169cc913ccbc9c80592ce3efd91fba
Mitchell Werksy <dungeonmasterwersky@gmail.com>
2020-12-12 14:28:57 -0800

    Nothing Major. THINGS WORKING

--- src/application/CarsApp.java
+++ src/application/CarsApp.java
@@ -1,10 +1,11 @@
 package application;
 
-public class Application { //THIS WILL NOT WORK WITHOUT ENTERING A PASSWORD IN THE DBconnection.java Class!
+public class CarsApp {
 
 	public static void main(String[] args) {
 		Menu menu = new Menu();
 		menu.start();
+
 	}
 
 }
